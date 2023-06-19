7a779c3bf3b1d33eea593bd1a33964db6036f444
AcrylicMadness <akirill1942@me.com>
2023-06-20 02:35:21 +0300

    Initial Commit

---
+++ BrewMongodStatus/BrewMongodStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewMongodStatusApp.swift
+//  BrewMongodStatus
+//
+//  Created by Кирилл Аверкиев on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct BrewMongodStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
